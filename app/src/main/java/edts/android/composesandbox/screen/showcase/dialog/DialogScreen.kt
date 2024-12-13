6d4ecf55bf3cbc8817c5b40f29cdedd45ba8b960
Ramdhan <muhammad.ramdhan@sg-edts.com>
2024-12-13 10:06:01 +0700

    FEAT(): Loading dialog

--- app/src/main/java/edts/android/composesandbox/screen/showcase/dialog/DialogScreen.kt
+++ app/src/main/java/edts/android/composesandbox/screen/showcase/dialog/DialogScreen.kt
@@ -3,28 +3,36 @@ package edts.android.composesandbox.screen.showcase.dialog
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -34,6 +42,7 @@ import edts.android.composesandbox.R
 import edts.android.composesandbox.screen.showcase.base.ShowcaseBaseScreen
 import edts.android.composesandbox.ui.theme.ComposeSandboxTheme
 import edts.android.composesandbox.util.LightDarkPreview
+import kotlinx.coroutines.delay
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -153,6 +162,52 @@ fun DialogScreen(
                     DatePicker(
                         state = pickerState
                     )
+                }
+            }
+        }
+        item {
+            var showDialog by remember { mutableStateOf(false) }
+            val duration = 3
+            var countDown by remember { mutableIntStateOf(duration) }
+            Button(
+                modifier = btnModifier,
+                onClick = {
+                    showDialog = true
+                    countDown = duration
+                }
+            ) {
+                Text("Loading Dialog")
+            }
+            AnimatedVisibility(showDialog) {
+                LaunchedEffect(showDialog) {
+                    for (i in duration downTo 1){
+                        delay(1000L)
+                        countDown = i-1
+                    }
+                    showDialog = false
+                }
+
+                Dialog(
+                    onDismissRequest = { showDialog = false }
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(8.dp))
+                        ){
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                        Text(
+                            "Dismissing in $countDown seconds",
+                            Modifier.padding(top = 16.dp),
+                            color = Color.White
+                        )
+                    }
                 }
             }
         }
